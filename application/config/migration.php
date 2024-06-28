5048b289c175aef3e19b61a47eb97a0d32323b28
Malcolm Rodrigues <rmalcolm911@gmail.com>
2024-06-28 13:11:19 +0530

    migration rename init file

--- application/config/migration.php
+++ application/config/migration.php
@@ -5,5 +5,5 @@ $config['migration_enabled'] = TRUE;
 $config['migration_type'] = 'timestamp';
 $config['migration_table'] = 'z_ci_migrations';
 $config['migration_auto_latest'] = FALSE;
-$config['migration_version'] = 0;
+$config['migration_version'] = 10000000000001;
 $config['migration_path'] = APPPATH . 'migrations/';
